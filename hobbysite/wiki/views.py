167200874d14f339df7cc1279492321db6072b45
shaunsulay3 <antonio.sulay@student.ateneo.edu>
2025-03-03 12:02:40 +0800

    edited articles_list to sort by created_on in descending order

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Article, ArticleCategory
 
 # Create your views here.
+
+def articles_list(request):
+    a = Article.objects.all().order_by('-created_on')
+    ctx = {
+        'articles': a
+    }
+    return render(request, 'wiki/articles_list.html',ctx)
+
+def article_detail(request, id):
+    a = Article.objects.get(id=id)
+    ctx = {
+        'article': a
+    }
+    
+    return render(request, 'wiki/article_detail.html', ctx)
+
