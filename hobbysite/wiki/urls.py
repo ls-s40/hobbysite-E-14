e0d2770f9481a47bd5ec6dd0608e28ca23e81021
shaunsulay3 <antonio.sulay@student.ateneo.edu>
2025-03-03 12:03:51 +0800

    added url routing

---
+++ hobbysite/wiki/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import articles_list, article_detail
+
+urlpatterns = [
+        path('articles/', articles_list, name='articles_list'),
+        path('article/<int:id>/', article_detail, name='article_detail')
+    ]
+
+app_name = "wiki"
