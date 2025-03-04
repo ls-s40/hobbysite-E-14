d783efd13c6c3c55e521b9f0e09891a50803f25a
evan <106312755+manreeq@users.noreply.github.com>
2025-03-04 15:14:36 +0800

    Created models and setup admin page

--- hobbysite/forum/admin.py
+++ hobbysite/forum/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post, PostCategory
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+
+class PostCategoryAdmin(admin.ModelAdmin):
+    model = PostCategory
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(PostCategory, PostCategoryAdmin)
--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.timezone import now
+from django.urls import reverse
 
 # Create your models here.
+class PostCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(PostCategory, on_delete=models.SET_NULL, null=True)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('forum:post_detail', args=[str(self.id)])
