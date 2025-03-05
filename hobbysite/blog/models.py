1507178062ea2ede8dbe141d873edbd36208a241
Martin <martin.mones@student.ateneo.edu>
2025-03-05 22:51:34 +0800

    made models and migrations

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('article_list', args=[self.id])
+    
+    class Meta:
+        ordering = ['name']
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True, related_name='articles')
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('article_detail', args=[self.id])
+
+    class Meta:
+        ordering = ['-created_on']
