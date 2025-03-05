9c85da0367ecde98ee00ecc5ceb04b7bf528ea13
Martin <martin.mones@student.ateneo.edu>
2025-03-05 23:22:12 +0800

    implemented views

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Article
 
-# Create your views here.
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'article_list.html', {'articles': articles})
+
+def article_detail(request, id):
+    article = get_object_or_404(Article, id=id)
+    return render(request, 'article_detail.html', {'article': article})
