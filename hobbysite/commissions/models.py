9fbf52386be83ea6635de66fd2229c7f647e4b13
iel2408 <gphermosura2017@gmail.com>
2025-03-06 20:24:34 +0800

    edited code according to pep8

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -1,7 +1,10 @@
+"""Models for commissions and comments"""
+
 from django.db import models
 from django.urls import reverse
 
 class Commission(models.Model):
+    """Commission model"""
     title = models.CharField(max_length=255)
     description = models.TextField()
     people_required = models.PositiveIntegerField()
@@ -9,20 +12,19 @@ class Commission(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return str(self.title)
 
     def get_absolute_url(self):
+        """Returns the absolute URL for a commission"""
         return reverse('commissions:commissions_detail', args=[str(self.id)])
-    
+
 
 class Comment(models.Model):
+    """Comment model"""
     commission = models.ForeignKey(Commission, on_delete=models.CASCADE)
     entry = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)  
-    updated_on = models.DateTimeField(auto_now=True) 
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'Comment on {self.commission.title} at {self.created_on}'
-
-
-
--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -1,9 +1,12 @@
+"""URL config for commissions"""
+
 from django.urls import path
 from .views import commissions_list, commissions_detail
 
+
 urlpatterns = [
         path('list/', commissions_list, name='commissions_list'),
-        path('detail/<int:id>/', commissions_detail, name='commissions_detail')
+        path('detail/<int:commission_id>/', commissions_detail, name='commissions_detail')
     ]
 
 app_name = "commissions"
