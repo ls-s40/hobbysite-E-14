9366881ab2bffa9ccaf19d68c9d6bf8115dc4389
evan <106312755+manreeq@users.noreply.github.com>
2025-03-06 21:59:47 +0800

    Edited to PEP8 standards

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,10 +1,13 @@
+"""Manages the different views of the forum app."""
+
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Post, PostCategory
 
 # Create your views here.
 
+
 def threads_list(request,):
+    """Return render of list view page using PostCategory model."""
     postcategories = PostCategory.objects.all().order_by('name')
     ctx = {
         'postcategories': postcategories
@@ -12,11 +15,13 @@ def threads_list(request,):
 
     return render(request, 'forum/threads_list.html', ctx)
 
+
 def thread_detail(request, id):
+    """Return render of detail view page using Post and PostCateogry models."""
     category = PostCategory.objects.get(id=id)
     posts = Post.objects.filter(category=category).order_by('-created_on').values()
     ctx = {
         'posts': posts,
-        'category':category
+        'category': category
     }
     return render(request, 'forum/thread_detail.html', ctx)
