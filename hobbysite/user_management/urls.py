3e7210a365acacc61787c08ed7ea698f8a024815
Martin <martin.mones@student.ateneo.edu>
2025-05-08 03:38:58 +0800

    fixed profile update view and templates

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import profile_update_view
 
 urlpatterns = [
-    path('profile/<str:username>/', profile_update_view, name='profile_update')
+    path('<str:username>/', profile_update_view, name='profile_update')
 ]
 
 app_name = 'user_management'
--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -7,13 +7,20 @@ from django.contrib.auth.models import User
 
 @login_required
 def profile_update_view(request, username):
+    """
+    View to display and update the user's profile.
+    """
     user = get_object_or_404(User, username=username)
     profile = get_object_or_404(Profile, user=user)
     if request.method == 'POST':
-        form = ProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            form.save()
-            return redirect('profile_update', username=username)
+        if 'edit' in request.POST:
+            form = ProfileForm(instance=profile)
+            return render(request, 'profile_form.html', {'form': form, 'profile': profile, 'is_editing': True})
+        elif 'save' in request.POST:
+            form = ProfileForm(request.POST, instance=profile)
+            if form.is_valid():
+                form.save()
+                return redirect('user_management:profile_update', username=username)
     else:
         form = ProfileForm(instance=profile)
-    return render(request, 'user_management/profile_form.html', {'form': form, 'profile': profile})
+    return render(request, 'profile_form.html', {'form': form, 'profile': profile, 'is_editing': False})
