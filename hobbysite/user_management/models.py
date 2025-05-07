a922128ad60665c790339c5306b39a4bc84ef9e3
Martin <martin.mones@student.ateneo.edu>
2025-05-08 00:42:21 +0800

    implemented models, urls, and views

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=63)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.display_name or self.user.username
--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import UpdateView
+from .models import Profile
+from .forms import ProfileForm
 
-# Create your views here.
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = Profile
+    form_class = ProfileForm
+    template_name = 'user_management/profile_form.html'
+    context_object_name = 'profile'
+
+    def get_object(self, queryset=None):
+        return Profile.objects.get(user=self.request.user)
+
