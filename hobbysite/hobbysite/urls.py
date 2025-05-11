c044e2af2bb5ba66d397481afb56771390cb7672
shaunsulay3 <antonio.sulay@student.ateneo.edu>
2025-05-11 23:12:44 +0800

    changed settings.py and urls.py to accommodate media directories. added media folder and subfolders inside it for each app, each with their own image folder.

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
     
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
@@ -30,3 +32,4 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('profile/', include(('user_management.urls', 'user_management'), namespace='user_management'))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
