e0217a24ad6c4b891c67193d79e680343d2be0a1
Danielle Sulay <lucia.sulay@student.ateneo.edu>
2025-05-11 18:19:32 +0800

    Edited Product and ProductType models

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -21,15 +21,40 @@ class ProductType(models.Model):
 class Product(models.Model):
     """Represents a Product Model."""
 
+    STATUS_CHOICES=[
+        ('available', 'Available'),
+        ('on_sale', 'On sale'),
+        ('out_of_stock', 'Out of stock'),
+    ]
+
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(null=True)
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='available'
+    )
     product_type = models.ForeignKey(
         ProductType,
         on_delete=models.SET_NULL,
         null=True,
-        related_name='products'
+        related_name='products',
+        editable=False
     )
+    owner=models.ForeignKey(
+        'user_management.Profile',
+        on_delete=models.CASCADE,
+        related_name='products',
+        null=True
+    )
+
+    def save(self, *args, **kwargs):
+        """Sets status to 'out_of_stock' when stock is 0."""
+        if self.stock == 0:
+            self.status = 'out_of_stock'
+        super().save(*args, **kwargs)
 
     def __str__(self):
         """Presents ProductType instances as human-readable representations."""
