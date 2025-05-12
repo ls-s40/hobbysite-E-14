f18286a25cfe1c0054113e5224c347e0270baf3a
shaunsulay3 <antonio.sulay@student.ateneo.edu>
2025-05-12 15:14:15 +0800

    formatted to fit pep8 standards

--- hobbysite/wiki/forms.py
+++ hobbysite/wiki/forms.py
@@ -1,22 +1,35 @@
-# app/forms.py
+"""This module contains form definitions for the wiki application."""
+
 from django import forms
 from .models import Article, Comment
 
+
 class ArticleForm(forms.ModelForm):
+    """A form for creating and updating Article instances."""
+
     class Meta:
+        """Meta options for the ArticleForm."""
+
         model = Article
-        fields = ['title', 'category', 'entry','header_image']
+        fields = ['title', 'category', 'entry', 'header_image']
+
 
 class CommentForm(forms.ModelForm):
+    """A form for creating and updating Comment instances."""
+
     class Meta:
+        """Meta options for the CommentForm."""
+
         model = Comment
         fields = ['entry']
         labels = {
-            'entry': ''
+            'entry': '',
         }
         widgets = {
-            'entry': forms.Textarea(attrs={
-                'rows': 4,
-                'placeholder': 'Write your comment here...',
-            }),
+            'entry': forms.Textarea(
+                attrs={
+                    'rows': 4,
+                    'placeholder': 'Write your comment here...',
+                }
+            ),
         }
--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,4 +1,4 @@
-"""Query database and render html page."""
+"""Query database and render HTML pages."""
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
@@ -7,90 +7,98 @@ from .forms import ArticleForm, CommentForm
 
 
 def articles_list(request):
-    """Query list of articles and render it as html page."""
-
+    """Query list of articles and render it as an HTML page."""
     user_is_authenticated = request.user.is_authenticated
     if user_is_authenticated:
         user_articles = Article.objects.filter(author=request.user)
     else:
         user_articles = []
+
     categories = ArticleCategory.objects.all()
     articles_categorized = []
     for category in categories:
         if user_is_authenticated:
-            articles = Article.objects.filter(category = category).exclude(author=request.user)
+            articles = Article.objects.filter(category=category).exclude(
+                author=request.user
+            )
         else:
-            articles = Article.objects.filter(category = category)
-        articles_categorized.append({ 
+            articles = Article.objects.filter(category=category)
+        articles_categorized.append({
             'category_name': category.name,
             'category_description': category.description,
-            'articles': articles
+            'articles': articles,
         })
-    
+
     ctx = {
         'user_articles': user_articles,
-        'articles_categorized': articles_categorized
+        'articles_categorized': articles_categorized,
     }
 
     return render(request, 'wiki/articles_list.html', ctx)
 
-def article_detail(request, id):
-    """Query article details and render it as html page."""
 
+def article_detail(request, id):
+    """Query article details and render it as an HTML page."""
     article = Article.objects.get(id=id)
     if request.method == 'POST':
-        commentForm = CommentForm(request.POST)
-        if commentForm.is_valid():
-            comment = commentForm.save(commit=False)
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
             comment.article = article
             comment.author = request.user
             comment.save()
             return redirect(request.get_full_path())
     else:
-        userIsAuthenticated = request.user.is_authenticated
-        userIsAuthor = request.user == article.author
-        relatedArticles = Article.objects.filter(category=article.category).exclude(id=article.id)[:3]
-        commentForm = CommentForm()
-        comments = Comment.objects.filter(article__id=id)
+        user_is_authenticated = request.user.is_authenticated
+        user_is_author = request.user == article.author
+        related_articles = Article.objects.filter(
+            category=article.category
+        ).exclude(id=article.id)[:3]
+        comment_form = CommentForm()
+        comments = Comment.objects.filter(article__id=id).order_by('-created_on')
         ctx = {
             'article': article,
-            'relatedArticles': relatedArticles,
+            'related_articles': related_articles,
             'comments': comments,
-            'commentForm': commentForm,
-            'userIsAuthenticated': userIsAuthenticated,
-            'userIsAuthor': userIsAuthor
+            'comment_form': comment_form,
+            'user_is_authenticated': user_is_authenticated,
+            'user_is_author': user_is_author,
         }
     return render(request, 'wiki/article_detail.html', ctx)
 
+
 @login_required
 def article_create(request):
-    """Query article details and render it as html page."""
+    """Create a new article and render the creation page."""
     if request.method == 'POST':
-        articleForm = ArticleForm(request.POST, request.FILES)
-        if articleForm.is_valid():
-            article = articleForm.save(commit=False)
+        article_form = ArticleForm(request.POST, request.FILES)
+        if article_form.is_valid():
+            article = article_form.save(commit=False)
             article.author = request.user
             article.save()
             return redirect(article.get_absolute_url())
     else:
-        articleForm = ArticleForm()
-        ctx = {
-            'articleForm' : articleForm
-        }
+        article_form = ArticleForm()
+
+    ctx = {
+        'article_form': article_form,
+    }
     return render(request, 'wiki/article_create.html', ctx)
+
 
 @login_required
 def article_update(request, id):
+    """Update an existing article and render the update page."""
     article = Article.objects.get(id=id)
     if request.method == 'POST':
-        articleForm = ArticleForm(request.POST, request.FILES, instance=article)
-        if articleForm.is_valid():
-            articleForm.save()
+        article_form = ArticleForm(request.POST, request.FILES, instance=article)
+        if article_form.is_valid():
+            article_form.save()
             return redirect(article.get_absolute_url())
     else:
-        articleForm = ArticleForm(instance=article)
-        ctx = {
-            'articleForm': articleForm
-        }
+        article_form = ArticleForm(instance=article)
 
+    ctx = {
+        'article_form': article_form,
+    }
     return render(request, 'wiki/article_update.html', ctx)
