7da24b56956ee099e8d07142bafb0126aa39efde
Martin <martin.mones@student.ateneo.edu>
2025-05-12 23:53:31 +0800

    more pep8 fixes

--- hobbysite/accounts/apps.py
+++ hobbysite/accounts/apps.py
@@ -1,6 +1,9 @@
+"""This file is used to configure the accounts app."""
 from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
+    """Configuration for accounts app."""
+    
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
