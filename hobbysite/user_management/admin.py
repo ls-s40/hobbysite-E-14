73fba3526a104b0e5fbe295ddfcf3bad9646bbba
Martin <martin.mones@student.ateneo.edu>
2025-05-12 23:50:13 +0800

    admin implementation and pep8 fix

--- hobbysite/user_management/admin.py
+++ hobbysite/user_management/admin.py
@@ -1,3 +1,15 @@
+"""This file contains admin configurations for the user_management app."""
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    """Admin interface for managing Profile model."""
+
+    list_display = ('user', 'display_name', 'email')
+    search_fields = ('user__username', 'display_name', 'email')
+    list_filter = ('user__is_active',)
+    ordering = ('user__username',)
+
+
+admin.site.register(Profile, ProfileAdmin)
--- hobbysite/user_management/apps.py
+++ hobbysite/user_management/apps.py
@@ -1,6 +1,9 @@
+"""This file is used to configure the user_management app."""
 from django.apps import AppConfig
 
 
 class UserManagementConfig(AppConfig):
+    """Configuration class for the user_management app."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_management'
